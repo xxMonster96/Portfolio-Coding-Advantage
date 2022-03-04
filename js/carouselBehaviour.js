046b2c312b3db573b99850cd8c126a2a2d5cf10d
xxMonster96 <99510649+xxMonster96@users.noreply.github.com>
2022-03-04 09:43:33 -0800

    Updated Java

--- js/carouselBehaviour.js
+++ js/carouselBehaviour.js
@@ -1,5 +1,5 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+window.onload = showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -13,16 +13,11 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("project fade");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
