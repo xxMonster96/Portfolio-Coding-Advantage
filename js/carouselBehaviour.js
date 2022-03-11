2cd14ae6e8220aefe12c93f0048d1be606a138f8
xxMonster96 <99510649+xxMonster96@users.noreply.github.com>
2022-03-11 00:18:53 -0800

    Holding Page Included

--- js/carouselBehaviour.js
+++ js/carouselBehaviour.js
@@ -1,9 +1,13 @@
 var slideIndex = 1;
-window.onload = showSlides(slideIndex);
+var buttonIndex = 1;
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+}
+
+function plusButtons(n) {
+  showButtons(buttonIndex += n);
 }
 
 // Thumbnail image controls
@@ -11,13 +15,36 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function currentButton(n) {
+  showButtons(buttonIndex = n);
+}
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("project fade");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
+}
+
+function showButtons(n) {
+  var i;
+  var buttons = document.getElementsByClassName("button_fade");
+  if (n > buttons.length) {
+    buttonIndex = 1
+  }
+  if (n < 1) {
+    buttonIndex = buttons.length
+  }
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].style.display= "none";
+  }
+  buttons[buttonIndex-1].style.display = "block";
 }
